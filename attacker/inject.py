331dd444eb7f5edc3afb46b918ea30f88f8750e2
JTaylor <67885009+JTaylor-Code@users.noreply.github.com>
2025-04-30 14:43:47 -0400

    Update inject.py

--- attacker/inject.py
+++ attacker/inject.py
@@ -6,10 +6,10 @@ PORT = 1883
 TOPIC = 'sensor/temperature'
 
 client = mqtt.Client(client_id='evil_sensor')  
-client.username_pw_set('sensor', 'password')   
+client.username_pw_set('sensor', 'password')  
+
 client.connect(BROKER, PORT, 60)
 client.loop_start()
-
 
 for i in range(5):
 	fake_temp = f"999.99 FAKE {i}"
